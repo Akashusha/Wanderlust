f42aad85c165fa9d8c7797cd1d07ed2d5c2b369f
Akashusha <74002257+Akashusha@users.noreply.github.com>
2024-10-07 16:53:46 +0530

    update

--- app.js
+++ app.js
@@ -64,9 +64,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+  return res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
